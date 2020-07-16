af45b6680d8b518b74ff0fc00d5985ad491c673d
neil <with@snow.com>
2020-07-16 11:09:17 +0800

    首页- 获取轮播图数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    //轮播图数组
+    swiperList:[]
   },
 
   /**
@@ -12,21 +13,32 @@ Page({
    */
   onLoad: function (options) {
     
-  },
+    var reqTask = wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result) => {
+        console.log(result)
+        this.setData({
+          swiperList: result.data.message
+        })
+      }
+    });
+      
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
   },
-
+  
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
     
   },
+  
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+      
+    },
 
   /**
    * 生命周期函数--监听页面隐藏
