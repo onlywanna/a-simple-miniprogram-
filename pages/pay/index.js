699c7a266f4c47ddbf8548ca44f6895f7483fdcf
neil <with@snow.com>
2020-07-23 16:42:22 +0800

    支付-数据展示

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,66 +1,59 @@
-// pages/pay/index.js
+/**
+ * 1. 页面加载需要做的事情
+ *    1 从缓存中获取购物车数据，渲染到页面中
+ *        这些数据的 isChecked 为 true
+ */
+
+
+import {
+  getSetting,
+  chooseAddress,
+  openSetting,
+  showModal,
+  showToast
+} from "../../utils/asyncWx.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  /*页面的初始数据*/
   data: {
+    address: {},
+    cart: [],
+    totalMoney: 0
+  },
+  onShow() {
+    const address = wx.getStorageSync('address')
+    this.setData({
+      address
+    })
+    let cart = wx.getStorageSync('cart') || []
 
+    //  过滤后的购物车数组
+    cart = cart.filter(v => v.isChecked)
+
+
+    // 下面是把setCart代码拿出来单独写了
+
+    // 1. 总价格  总数量
+    let totalMoney = 0
+    let totalNum = 0
+    cart.forEach(v => {
+      if (v.isChecked) {
+        totalMoney += v.num * v.goods_price
+        totalNum += v.num
+      }
+    })
+    // 判断数组是否为空？
+    this.setData({
+      totalMoney,
+      totalNum,
+    })
+
+    this.setData({
+      cart
+    })
+    wx.setStorageSync('cart', cart)
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
-  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
