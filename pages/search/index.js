70d7101a7b8d2084ac2a21de41ce5bd550e4ea3b
neil <with@snow.com>
2020-07-28 10:09:09 +0800

    搜索中心-防抖的实现

--- pages/search/index.js
+++ pages/search/index.js
@@ -4,6 +4,8 @@
 //     3. 检验通过 把输入框的值 发送到后台
 //     4. 返回的数据打印到页面上
 
+// 2.防抖 (防止抖动) 定时器
+      // 1.定义全局的定时器id
 import {request} from "../../request/index"
 Page({
 
@@ -13,6 +15,7 @@ Page({
   data: {
     search_goods: []
   },
+  TimeId:-1,
   // 输入框的值改变, 就会触发的事件
   handleInput(e){
     // 1.获取输入框的值
@@ -22,7 +25,10 @@ Page({
       // 值不合法
       return
     // 3. 准备发送请求，获取数据
-    this.qsearch(value)
+    clearTimeout(this.TimeId);
+    this.TimeId = setTimeout(() => {
+      this.qsearch(value)
+    }, 1000);
     },
 
   // 发送请求获取搜索建议  数据
