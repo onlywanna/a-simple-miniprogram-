63acdcd567db25fa2d09918dde2d74b1e99cae2f
neil <with@snow.com>
2020-07-28 09:55:37 +0800

    搜索中心-渲染搜索结果

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,36 @@
-// pages/search/index.js
+// 1. 输入框绑定 值改变事件 input事件
+//     1. 获取到输入框的值
+//     2.合法性判断
+//     3. 检验通过 把输入框的值 发送到后台
+//     4. 返回的数据打印到页面上
+
+import {request} from "../../request/index"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    search_goods: []
   },
+  // 输入框的值改变, 就会触发的事件
+  handleInput(e){
+    // 1.获取输入框的值
+    const {value} = e.detail
+    // 2. 检测合法性
+    if(!value.trim())
+      // 值不合法
+      return
+    // 3. 准备发送请求，获取数据
+    this.qsearch(value)
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 发送请求获取搜索建议  数据
+  async qsearch(query){
+    const res = await request({url:"/goods/qsearch",data:{query}})
+    this.setData({
+      search_goods:res
+    })
   }
+    
 })
